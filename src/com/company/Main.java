08128951fa833e65a13b892df2d1c4c1bf864b44
shihaka <att253@ya.ru>
2020-12-10 16:29:43 +0400

    asdf

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	int a = 256;
-	      for (int i = 0; i <= a; i++){
-	          if (i * i == a)
-	              System.out.print(i);
-          }
+	      for (int i = 0; i <= a; i++)
+			  if (i * i == a)
+				  System.out.print(i);
     }
 }
