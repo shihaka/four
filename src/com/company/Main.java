5034243c6cdb4b7b7bc8603f4f2a1e0d640712f8
shihaka <att253@ya.ru>
2020-12-10 16:21:32 +0400

    sqrt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	char a = 'z';
-	for (char i = 'a'; i <= a; i++){
-        System.out.print(i);
-       }
+	int a = 256;
+	      for (int i = 0; i <= a; i++){
+	          if (i * i == a)
+	              System.out.print(i);
+          }
+
     }
 }
